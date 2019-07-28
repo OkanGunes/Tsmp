5d342999d3ee1ce3eaa83ed2f24aeaa147a308b0
Okan Güneş <okan.gunes@outlook.com>
2019-07-28 21:42:29 +0300

    User API,Domain and Infrastructure projects created

---
+++ src/Services/User/Tsmp.User.Domain/UserAggregate/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tsmp.User.Domain.UserAggregate
+{
+    public class UserEntity 
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
