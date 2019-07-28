7c1c7513ddb6547492d854aa7ea8415ce0fd9f70
Okan Güneş <okan.gunes@outlook.com>
2019-07-28 22:00:18 +0300

    PasswordResetEntity and IPasswordResetRepository created

---
+++ src/Services/User/Tsmp.User.Domain/PasswordResetAggregate/PasswordResetEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tsmp.User.Domain.PasswordResetAggregate
+{
+    public class PasswordResetEntity
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+    }
+}
