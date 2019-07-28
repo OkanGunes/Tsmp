61fbe2b5ad37427b666498b1fa253736d741b543
Okan Güneş <okan.gunes@outlook.com>
2019-07-28 21:57:15 +0300

    IUserRepository, PasswordEntity and IPasswordRepository created

---
+++ src/Services/User/Tsmp.User.Domain/PasswordAggregate/IPasswordRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tsmp.User.Domain.PasswordAggregate
+{
+    public interface IPasswordRepository
+    {
+    }
+}
---
+++ src/Services/User/Tsmp.User.Domain/PasswordAggregate/PasswordEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tsmp.User.Domain.PasswordAggregate
+{
+    public class PasswordEntity
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Hash { get; set; }
+
+        public string Salt { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
