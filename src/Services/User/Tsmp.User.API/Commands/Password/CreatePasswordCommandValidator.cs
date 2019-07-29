b5b7e508fa221862624cfff95166be00ca42933c
Okan Güneş <okan.gunes@outlook.com>
2019-07-29 21:27:14 +0300

    CreatePasswordCommandValidator and CreateUserCommandValidator created.

---
+++ src/Services/User/Tsmp.User.API/Commands/Password/CreatePasswordCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tsmp.User.API.Commands
+{
+    public class CreatePasswordCommandValidator : AbstractValidator<CreatePasswordCommand>
+    {
+        public CreatePasswordCommandValidator()
+        {
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .Length(7, 25);
+
+            RuleFor(x => x.UserId)
+                .NotEmpty();
+        }
+    }
+}
---
+++ src/Services/User/Tsmp.User.API/Commands/User/CreateUserCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tsmp.User.API.Commands
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(20);
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .Length(7, 20);
+
+            RuleFor(x => x.Surname)
+                .NotEmpty()
+                .MaximumLength(20);
+        }
+    }
+}
